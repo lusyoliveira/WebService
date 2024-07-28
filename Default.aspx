4dafe2627ec8fbf9a3a93d32abe2f724c9a882c2
Luciene Oliveira <Luciene Oliveira@CASA2>
2024-07-28 19:22:13 -0300

    Subindo Projeto

---
+++ Default.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Label ID="Label1" runat="server" Text="Valor A:"></asp:Label>
+&nbsp;<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label2" runat="server" Text="Valor B:"></asp:Label>
+&nbsp;<asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label3" runat="server" Text="Operação:"></asp:Label>
+&nbsp;<asp:DropDownList ID="DropDownList1" runat="server">
+            <asp:ListItem>Adição</asp:ListItem>
+            <asp:ListItem>Divisão</asp:ListItem>
+            <asp:ListItem>Multiplicação</asp:ListItem>
+            <asp:ListItem>Subtração</asp:ListItem>
+        </asp:DropDownList>
+        <br />
+        <asp:Label ID="Label4" runat="server" Text="Resultado:"></asp:Label>
+&nbsp;<asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+        <br />
+        <asp:Button ID="Button1" runat="server" onclick="Button1_Click" 
+            Text="Calcular" />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        {
+            decimal ValorA;
+            decimal ValorB;
+            Calculadora.OperacoesBasicas operacao = Calculadora.OperacoesBasicas.Adicao;
+            decimal Resultado;
+            decimal.TryParse(TextBox1.Text, out ValorA);
+            decimal.TryParse(TextBox2.Text, out ValorB);
+            switch (DropDownList1.SelectedIndex)
+            {
+                case 0:
+                    operacao = Calculadora.OperacoesBasicas.Adicao;
+                    break;
+                case 1:
+                    operacao = Calculadora.OperacoesBasicas.Divisao;
+                    break;
+                case 2:
+                    operacao = Calculadora.OperacoesBasicas.Multiplicacao;
+                    break;
+                case 3:
+                    operacao = Calculadora.OperacoesBasicas.Subtracao;
+                    break;
+                default:
+                    operacao = Calculadora.OperacoesBasicas.Adicao;
+                    break;
+            }
+            Calculadora.Service MeuWebService = new Calculadora.Service();
+            Resultado = MeuWebService.Calculadora(ValorA, ValorB, operacao);
+            TextBox3.Text = Resultado.ToString();
+        }
+    }
+}
